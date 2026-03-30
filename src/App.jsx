31e69b2dbbf840c866e4fe60b2e8ae003ee00801
Brian Ochieng <bochieng228@gmail.com>
2026-03-30 19:45:14 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+//import WhatsApp from './components/WhatsApp';
+import Home from './pages/Home';
+export default function App (){
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+import WhatsApp from "../components/WhatsApp"
+
+export default function Home (){
+    return(
+        <>
+        <WhatsApp />
+        </>
+    )
+}
